04e3f420d912b5989ea9cc56c1253617416969a5
Md-Ali-Azad <aliazad969@gmail.com>
2019-05-12 22:43:01 +0600

    inistialization_of_my_project

---
+++ main_lms/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MainLmsConfig(AppConfig):
+    name = 'main_lms'
---
+++ main_lms/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .import views 
+urlpatterns = [  
+    #path('admin/', admin.site.urls),
+    #path('', views.show, name="show"),  
+   # path('emp/', views.emp, name="emp"),  
+]  
